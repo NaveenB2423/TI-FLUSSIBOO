7a7a86ad526ccfb0d9bf273980d5ef8b899453d1
vimurgx-neo <vimurgxneo@gmail.com>
2026-08-30 19:47:07 +0530

    Configure WhiteNoise and staticfiles for Render production deployment

--- trendylooms/settings.py
+++ trendylooms/settings.py
@@ -77,6 +77,13 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
+try:
+    import whitenoise  # noqa: F401
+    MIDDLEWARE.insert(1, 'whitenoise.middleware.WhiteNoiseMiddleware')
+    STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+except ImportError:
+    pass
+
 ROOT_URLCONF = 'trendylooms.urls'
 
 TEMPLATES = [
@@ -189,11 +196,11 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
 MEDIA_URL = '/media/'
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
-STATIC_ROOT = os.path.join(BASE_DIR, "static/")
+STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles/")
 
 USER_IMAGE_UPLOAD_PATH = 'profile/'
 PRODUCT_IMAGE_UPLOAD_PATH = 'product/'
